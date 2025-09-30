1a606b22dcc2f3dd211be8cc2c8b2c425621f4ae
Sanek <a.z10293841@gmail.com>
2025-09-30 20:50:54 +0400

    Сделана бд

---
+++ server/models/Order.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+const User = require("./User");
+
+const Order = sequelize.define("Order", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  user_id: { type: DataTypes.INTEGER, allowNull: false, references: { model: User, key: "id" } },
+  total_price: { type: DataTypes.FLOAT, allowNull: false },
+  status: { type: DataTypes.ENUM("pending", "shipped", "delivered"), allowNull: false },
+});
+
+// Один заказ принадлежит одному пользователю
+// Один заказ может содержать несколько товаров через OrderItem
+
+module.exports = Order;
