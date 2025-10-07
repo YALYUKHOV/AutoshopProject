e531be2fce8c0d2fc059a6988c9d5a394639a433
Sanek <a.z10293841@gmail.com>
2025-10-07 13:26:00 +0400

    часть контр юзер, роуты, error middleware

---
+++ server/controllers/cartController.js
@@ -0,0 +1,8 @@
+const { Cart, CartItem, Product } = require("../models/models");
+const ApiError = require("../error/APIError");
+
+class CartController {
+
+}
+
+module.exports = new CartController();
