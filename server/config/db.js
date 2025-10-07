fb314ca9edd41dad3ecac9cc3763d473e582e8fe
Kamilla <kamillazabbarova28@gmail.com>
2025-10-07 14:57:37 +0400

    Добавлена папка server/config для настроек БД и утилит, необходимые для реализации эндпоинтов регистрации и входа

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+// конфигурация подключения к БД
+const { Sequelize } = require("sequelize"); // импорт, со взятием конкретного класса Sequelize (Деструктуризация)
+
+const config = require("../config/config.json")[process.env.NODE_ENV || "development"];
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME || config.database,
+  process.env.DB_USER || config.username,
+  process.env.DB_PASSWORD || config.password,
+  {
+    host: process.env.DB_HOST || config.host,
+    port: process.env.DB_PORT || config.port,
+    dialect: config.dialect,
+    logging: false,
+  }
+);
+
+module.exports = sequelize;
